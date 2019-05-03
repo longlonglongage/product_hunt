40b17f95fdcc9e33dda6bcd3f85bc1cef0e5fcc0
Coconut_Husk <1147583895@qq.com>
2019-05-03 23:32:52 +0800

    基本完成了APP Model和后端逻辑

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(default='例：抖音-用短视频记录美好生活', max_length=50)
+    intro = models.TextField(default='在这里写APP介绍')
+    urls = models.CharField(default='HTTP://', max_length=100)
+    icon = models.ImageField(default='boy.png', upload_to='images/')
+    image = models.ImageField(default='default.png', upload_to='images/')
+
+    votes = models.IntegerField(default=1)
+    pub_date = models.DateTimeField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
